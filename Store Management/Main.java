572134d8b3f00408740d7c1ff7fa301836110ead
Zach McMullen <zachmcmullen04@gmail.com>
2022-12-11 21:52:44 -0800

    init

--- Store Management/Main.java
+++ Store Management/Main.java
@@ -1,13 +1,101 @@
 import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Welcome To The Management System!");
-        System.out.println("Enter The Following Numbers To Trigger An Action");
-        System.out.println("1 To ddd a new product \n2 To edit an existing product \n3 To process an order \n4 To add a new Customer \n0 To end the program");
-        if (input.nextInt() == 1) {
-            System.out.println("Press 1 to add a shoe \nPress 2 to add new clothing");
+public class MyConsole {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    //array of objects
+    Product[] productList = new Product[4];
+    
+    //products (objects)
+    Shirt kobeTee = new Shirt("Kobe Graphic Tee", 45, "Nike", "Yellow", "M", true);
+    Shirt uscTee = new Shirt("USC Workout Shirt", 20, "Jordan", "Maroon", "L", false);
+    Shoe fluGame = new Shoe("Joran 12 Flu Game", 250, "Jordan", "Red/Black", 12.5, "streetwear");
+    Shoe kobeGrinch = new Shoe("Kobe 6 Grinch", 650, "Nike", "Green", 12, "basketball");    
+    
+    productList[0] = kobeTee;
+    productList[1] = uscTee;
+    productList[2] = fluGame;
+    productList[3] = kobeGrinch;
+
+    //main loop asking for customer interaction
+    int loopCount = 0;
+    int userInput = 0;
+    System.out.println(" ");
+    System.out.println("Welcome to the store!");
+    System.out.println("Respond to the following prompts. Type -1 if you want to quit");
+    while (userInput != -1) {
+      if (loopCount == 0) {
+        System.out.println("What product would you like to take a look at?");
+      } else {
+        System.out.println(" ");
+        System.out.println("Would you like to keep looking?");
+        System.out.println("Type 1 for yes, -1 for no");
+        System.out.println(" ");
+        userInput = input.nextInt();
+        if (userInput == -1) {
+          return;
         }
+        System.out.println(" ");
+        System.out.println("What product would you like to take a look at?");
+      }
+      loopCount++;
+      System.out.println("Type the number associated with the product you want to look at");
+      System.out.println(" ");
+      for (int i = 0; i < productList.length; i++) {
+        System.out.println(i + ": " + productList[i].getName());
+        System.out.println(" ");
+      }
+      userInput = input.nextInt();
+      if (userInput != -1) {
+        int selectedIndex = userInput;
+        System.out.println(" ");
+        System.out.println(productList[selectedIndex]);
+        System.out.println(" ");
+        System.out.println("Would you like to buy this product?");
+        System.out.println("Type 1 for yes, 2 for no, -1 to quit");
+        System.out.println(" ");     
+        userInput = input.nextInt();
+        if (userInput == 1) {
+          System.out.println(" ");
+          System.out.println("Would you like to negotiate the price?");
+          System.out.println("Type 1 for yes, 2 for no, -1 to quit");
+          System.out.println(" ");
+          userInput = input.nextInt();
+          if (userInput == 1) {
+            System.out.println(" ");
+            System.out.println("What is your offer?");
+            System.out.println(" ");
+            userInput = input.nextInt();
+            if (productList[selectedIndex].negotiatePrice(userInput)) {
+              System.out.println(" ");
+              System.out.println("We accept your offer!");
+              System.out.println("You bought " + productList[selectedIndex].getName() + " for " + productList[selectedIndex].getPrice());
+              System.out.println(" ");
+            } else {
+              System.out.println(" ");
+              System.out.println("Unfortunately we cannot accept your offer");
+              System.out.println("Would you still like to purchase the " + productList[selectedIndex].getName() + "?");
+              System.out.println("Type 1 for yes, 2 for no, -1 to quit");
+              System.out.println(" ");
+              userInput = input.nextInt();
+              if (userInput == 1) {
+                System.out.println(" ");
+                System.out.println("You bought " + productList[selectedIndex].getName() + " for " + productList[selectedIndex].getPrice());
+                System.out.println(" ");                
+              } else if (userInput == 2) {
+                System.out.println(" ");
+                System.out.println("We are very sorry to hear that!");
+                System.out.println(" ");
+              }
+            }
+          } else if (userInput == 2) {
+            System.out.println(" ");
+            System.out.println("You bought " + productList[selectedIndex].getName() + " for " + productList[selectedIndex].getPrice());
+            System.out.println(" ");
+          }
+        }
+      }
     }
+  }
 }
--- Store Management/Product.java
+++ Store Management/Product.java
@@ -5,53 +5,59 @@ public class Product {
     private String color;
   
     public Product() {
-        name = "";
-        price = 0;
-        brand = "";
-        color = "";
+      name = "";
+      price = 0;
+      brand = "";
+      color = "";
     }
   
     public Product(String name, double price, String brand, String color) {
-        this.name = name;
-        this.price = price;
-        this.brand = brand;
-        this.color = color;
+      this.name = name;
+      this.price = price;
+      this.brand = brand;
+      this.color = color;
     }
   
     public String getName() {
-        return name;
+      return name;
     }
   
     public double getPrice() {
-        return price;
+      return price;
     }
-
+  
     public String getBrand() {
-        return brand;
+      return brand;
     }
-
+  
     public String getColor() {
-        return color;
+      return color;
     }
   
     public void setName(String name) {
-        this.name = name;
+      this.name = name;
     }
   
-    public void setPrice(double price) {
-        this.price = price;
+    public boolean negotiatePrice(double newPrice) {
+      if (newPrice >= price - (price * 0.2)) {
+        price = newPrice;
+        return true;
+      } else {
+        return false;
+      }
     }
-
+  
     public void setBrand(String brand) {
-        this.brand = brand;
+      this.brand = brand;
     }
-
+  
     public void setColor(String color) {
-        this.color = color;
+      this.color = color;
     }
   
     public String toString() {
-        String data = "Product Name: " + name + "\nProduct Price: " + price + "\nBrand: " + brand + "\nColor: " + color;
-        return data;
+      String data = "Product Name: " + name + "\nProduct Price: " + price + "\nBrand: " + brand + "\nColor: " + color;
+      return data;
     }
-}
+  }
+  
